46581a8b22f797a4ebfb9ae25becde2b4f7eb0bc
Keerthana <keerthanans208@gmail.com>
2026-04-07 23:46:06 -0700

    this is my first commit

---
+++ functions.js
@@ -0,0 +1,18 @@
+let user="keerthana";
+function greet(u){
+    console.log(`hello ${u}`);
+    return u;
+}
+let username=greet(user);
+console.log(username)
+
+let add =()=>{
+    let a=10,b=30;
+    let result=b+a;
+    console.log(result)
+    return result;
+
+}
+let sum=add();
+sum=sum+sum;
+console.log(sum);
